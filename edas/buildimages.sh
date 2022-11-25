2e7e0abfa813e9ae599ecc5ca043043d12dd02a8
alickreborn <alickreborn@gmail.com>
2022-11-25 22:29:00 +0800

    Support Open Ananolis.

--- edas/buildimages.sh
+++ edas/buildimages.sh
@@ -52,9 +52,36 @@ function build_tomcat {
   #docker push apaas/edas:latest
 }
 
+function build_anolis {
+  TOMCAT_VERSION=${1}
+  TOMCAT_LABEL=${2}
+
+  ENDPOINT=http://edas-hz.oss-cn-hangzhou.aliyuncs.com
+  TOMCAT_LOCATION=${ENDPOINT}/agent/prod/files/apache-tomcat-${TOMCAT_VERSION}.tar.gz
+
+  TOMCAT_FILE="build/tmp/tomcat.tar.gz"
+
+  mkdir -p build/tmp
+  wget ${TOMCAT_LOCATION} -O  ${TOMCAT_FILE}
+
+  docker build -f openanolis/Dockerfile ./ \
+    --build-arg TOMCAT_LOCATION=${TOMCAT_FILE} \
+    --build-arg TOMCAT_FULLNAME=apache-tomcat-${TOMCAT_VERSION} \
+    -t apaas/edas-openanolis-openjdk8-${TOMCAT_LABEL}:${TOMCAT_VERSION} \
+    -t apaas/edas-openanolis-openjdk8-${TOMCAT_LABEL}:latest \
+    -t apaas/edas-openanolis:latest
+
+  #docker push apaas/edas-centos-openjdk8-${TOMCAT_LABEL}:${TOMCAT_VERSION}
+  #docker push apaas/edas-centos-openjdk8-${TOMCAT_LABEL}:latest
+  #docker push apaas/edas:latest
+}
+
+
 
 build_alitomcat "3.5.9" "7.0.92" "alitomcat7"
 build_alitomcat "3.5.9" "8.5.37" "alitomcat8"
 
 build_tomcat "7.0.93" "tomcat7"
 build_tomcat "8.5.42" "tomcat8"
+build_anolis "7.0.93" "tomcat7"
+build_anolis "8.5.42" "tomcat8"
